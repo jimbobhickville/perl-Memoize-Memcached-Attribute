e7436fb6b00944befa77e8703864b842e0d57534
Greg Hill <gnhill@liquidweb.com>
2012-06-11 13:26:37 -0400

    use Dist::Zilla plugins for some boilerplate, add more POD

--- lib/Memoize/Memcached/Attribute.pm
+++ lib/Memoize/Memcached/Attribute.pm
@@ -1,4 +1,5 @@
 package Memoize::Memcached::Attribute;
+# ABSTRACT: auto-memoize function results using memcached and subroutine attributes
 
 use strict;
 use warnings;
@@ -8,7 +9,7 @@ use Sub::Attribute;
 use Digest::MD5 ();
 use Storable ();
 
-our $VERSION = '0.10'; # VERSION
+our $VERSION = '0.11'; # VERSION
 our $MEMCACHE;
 our %CLIENT_PARAMS = (
 	servers => ['127.0.0.1:11211']
@@ -142,31 +143,38 @@ sub _build_key {
 1;
 __END__
 
-=head1 NAME
-
-Memoize::Memcached::Attribute - auto-memoize function results using memcached
-
-=head1 VERSION
-
-version 0.10
-
 =head1 SYNOPSIS
 
-To set up your memcache client for this package, you can pass the params in during import:
+If you're running memcache on your local box, with the default port, you can initialize without passing any
+parameters:
 
-	use Memoize::Memcached::Attribute (servers => [ '127.0.0.1:11211' ]);
+	use Memoize::Memcached::Attribute;
+
+This will use the default server list of 127.0.0.1:11211
+
+If you want to specify the constructor parameters for your Cache::Memcached or Cache::Memcached::Fast client object,
+you can pass them in during import:
+
+	use Memoize::Memcached::Attribute (
+		servers => [ '192.168.1.2:11211', '192.168.1.3:11211' ],
+		_connect_timeout => 0.1,
+		max_failures => 5,
+	);
 
 Alternatively, you can pass in your memcache client object entirely (we use this because
 we subclass Cache::Memcached::Fast to add some additional methods and default parameters):
 
-	use Memoize::Memcached::Attribute (-client => Cache::Memcached::Fast->new({ servers => [ '127.0.0.1:11211' ] }));
+	use Memoize::Memcached::Attribute (-client => Cache::Memcached::Fast->new(\%some_params));
 
 Or you can specify it at runtime, the only caveat being that you must do this prior to calling any memoized function:
 
 	use Memoize::Memcache::Attribute;
-	Memoize::Memcache::Attribute::client(Cache::Memcached::Fast->new({ servers => [ '127.0.0.1:11211' ] }));
+	Memoize::Memcache::Attribute::reset(Cache::Memcached::Fast->new(\%some_params));
 
-To use the memoization, you use the :CacheMemoize subroutine attribute, which takes a cache duration as a parameter:
+And that's basically it.  Now you have a :CacheMemoize subroutine attribute that will memoize subroutine results
+based on their parameters, storing the memoized data in memcache for a specified duration.
+
+To use the memoization, you just pass your cache duration to the :CacheMemoize subroutine attribute:
 
 	# cache the results in memcache for 5 minutes
 	sub myfunc :CacheMemoize(300) {
@@ -176,8 +184,9 @@ To use the memoization, you use the :CacheMemoize subroutine attribute, which ta
 		return $total;
 	}
 
-Sometimes you have a method that is not dependent on object state, and you want to memoize those results,
-independent of the object used to generate them:
+Sometimes you have an object method that is not dependent on object state, and you want to memoize those results,
+independent of the object used to generate them.  So we provide that option by passing in 'method' as your first
+parameter with the cache duration as the second:
 
 	# cache the results in memcache for 30 seconds
 	# but don't look at the object as part of the input data
@@ -186,6 +195,8 @@ independent of the object used to generate them:
 		my @params = @_;
 		return join('.', @params);
 	}
+
+Really, you can pass anything in as a first parameter and it will be ignored if it isn't case-insensitively equal to 'method'.
 
 While not generally recommended as good design, we do support the ability to
 invalidate caches.  If you find yourself using the invalidation often, this module
@@ -199,29 +210,52 @@ is probably the only place you should be), you can omit the package name:
 
 	Memoize::Memcached::Attribute::invalidate('mymethod', @params);
 
-
-
 =head1 DESCRIPTION
 
 Memoization is a process whereby you cache the results of a function, based on its input,
-in memory.  This module expands that concept to use memcache to provide a shared memory
-cache rather than a per-process cache like a lot of other memoization modules.  You can
-also specify a timeout, in case your results might change, just not that frequently.
+in memory so that repeated calls to the same function don't waste cycles recalculating the results.  Generally you use
+it with functions that are somewhat expensive to run (or that you have to run so frequently they become expensive), and that
+always return the same results based on the same input (i.e. they have no side effects).  This module expands that concept to
+use memcache to provide a shared memory cache, rather than a per-process cache like a lot of other memoization modules, so
+that multiple processes can reuse the results.  It gives you the added benefit that the memoization is not permanent, because
+you specify a timeout on the cached data.  So, if you have a method that has no side effects, but the data it's returning might
+become stale, you can still get the benefits of memoization while also having it automatically recalculate the results
+from time to time.
+
+=head1 METHODS
+
+=method reset
+
+Allows you to reset the package global memcache client after forking.
+
+=method invalidate
+
+Allows you to invalidate cached data.
+
+=method import
+
+Allows you to specify memcache connection parameters or your own client object to be used.
+
+=method CacheMemoize
+
+Should not be called directly.  This is the subroutine attribute handler exported by this package.
 
 =head1 OPTIONS
 
 When you import the package, you can pass a few options in:
 
-=over4
+=over 4
 
-=item -noattrimport - Precludes importing the necessary methods to the calling namespace
+=item -noattrimport - By default, we import some methods to make the attribute work properly in subclasses.
+This flag prevents that behavior.  It allows you to avoid cluttering your namespace a little, at the expense of
+not working with inheritance.
 
 =item -client - Allows you to specify your own memcache client object.  Useful if you subclass
 Cache::Memcached in your codebase.
 
 =back
 
-Any remaining options will be used to connect to the Cache::Memcached client object, if passed.
+Any remaining options will be used to _connect to the Cache::Memcached client object, if passed.
 
 =head1 THREADS/FORKING
 
@@ -247,17 +281,10 @@ LiquidWeb for allowing me to contribute this to CPAN.
 =head1 BUGS
 
 None known.  This has been in use in LiquidWeb production code for a few years without any known issues.
+It was slightly modified to remove some dependence on other LiquidWeb code, so there's an extremely
+remote chance that something broke in the process.
 
-If you find one, or have a feature request, submit them here:
+If you find a bug, or have a feature request, submit it here:
 https://github.com/jimbobhickville/perl-Memoize-Memcached-Attribute/issues/new
-
-=head1 LICENCE
-
-Copyright 2010-2012, Greg Hill (jimbobhickville -AT- gmail -DOT- com)
-
-This software is free.  It is licensed under the same terms as Perl itself.
-
-The latest version of this software should be available from my github repo:
-https://github.com/jimbobhickville/perl-Memoize-Memcached-Attribute
 
 =cut
